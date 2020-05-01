06f28c88509a48934f8d21eb3df7ed847c8b8f94
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-04-30 21:45:36 -0700

    Task queue settings can be overwritten with report_settings.

--- application/create_queue_task.py
+++ application/create_queue_task.py
@@ -11,21 +11,23 @@ REPORT_QUEUE = app.config.get('REPORT_QUEUE')
 client = tasks_v2.CloudTasksClient()
 
 
-def _get_report_queue(queue_name):
+def _get_report_queue(queue_name, report_settings):
     """ Creates or gets a queue to process the outcome of capture queues processed by this application.
         May need to refactor to Create or Update a queue.
     """
-    if not queue_name:
-        queue_name = 'test'
+    # report_settings = {'service': environ.get('GAE_SERVICE', 'dev'), 'relative_uri': '/capture/report/'}
+    queue_name = report_settings.get('queue_name', queue_name or 'test')
     queue_name = f"{REPORT_QUEUE}-{queue_name}".lower()
     parent = client.location_path(PROJECT_ID, PROJECT_REGION)  # f"projects/{PROJECT_ID}/locations/{PROJECT_REGION}"
     queue_path = client.queue_path(PROJECT_ID, PROJECT_REGION, queue_name)
-    routing_override = {'service': REPORT_SERVICE}
     rate_limits = {'max_concurrent_dispatches': 1, 'max_dispatches_per_second': 1}
+    rate_limits.update(report_settings.get('rate_limits'))
     retry_config = {'max_attempts': 30, 'min_backoff': '1', 'max_backoff': '3600', 'max_doublings': 10}
     retry_config['max_retry_duration'] = '48h'
-    queue_settings = {'name': queue_path, 'app_engine_routing_override': routing_override, 'rate_limits': rate_limits}
-    queue_settings['retry_config'] = retry_config
+    retry_config.update(report_settings.get('retry_config'))
+    queue_settings = {'name': queue_path, 'rate_limits': rate_limits, 'retry_config': retry_config}
+    if report_settings.get('service'):
+        queue_settings['app_engine_routing_override'] = {'service': report_settings['service']}
     for queue in client.list_queues(parent):  # TODO: Improve efficiency since queues list is in lexicographical order
         if queue_settings['name'] == queue.name:
             # TODO: Fix q = client.update_queue(queue_settings, retry=capture_retry)
@@ -48,46 +50,23 @@ def _get_report_queue(queue_name):
     return queue_path if q else None
 
 
-def add_to_report(media_type, media_id, answer, queue_name='testing', task_name=None, in_seconds=90):
+def add_to_report(payload, report_settings, queue_name='testing', task_name=None, in_seconds=90):
     """ Will add a task to a Capture Queue with a POST request if given a payload, else with GET request. """
     if not isinstance(task_name, (str, type(None))):
-        raise TypeError("Usually the task_name for add_to_capture should be None, but should be a string if set. ")
-    if not isinstance(media_type, str) or not isinstance(media_id, (int, str)):
-        raise TypeError("At least one of the first two parameters in the add_to_report function where the wrong type. ")
-    if not isinstance(answer, dict):
-        raise TypeError("Expected a dictionary for the third parameter in add_to_report. ")
-    payload = answer.copy()
-    parent = _get_report_queue(queue_name)
-    report_api_path = f"/capture/report/{str(media_id)}/"
-    # report_api_path += f"?url={str(post.permalink)}"
-    if len(answer['deleted']) == len(answer['file_list']) + 1:
-        del payload['deleted']
-    else:
-        # TODO: determine which files have issues & handle them.
-        pass
-    if len(answer['error']) == 0:
-        del payload['error']
-    else:
-        # TODO: determine which files have issues & handle them.
-        pass
-    if len(answer['file_list']) == len(answer['url_list']):
-        del payload['file_list']
-    else:
-        # TODO: determine which files have issues & handle them.
-        pass
-    # payload now has key for 'success', 'message', and 'url_list'
-    change_vals = {'media_id': str(media_id), 'media_type': media_type.lower()}
-    change_vals['saved_media'] = payload.pop('url_list', [])
-    payload['changes'] = [change_vals]
-    http_method = 'POST' if payload else 'GET'
+        raise TypeError("Usually the task_name for add_to_report should be None, but should be a string if set. ")
+    if not isinstance(payload, dict):
+        raise TypeError("Expected a dictionary for the first parameter in add_to_report. ")
+    # report_settings = {'service': environ.get('GAE_SERVICE', 'dev'), 'relative_uri': '/capture/report/'}
+    # payload = {'success': Bool, 'message': '', 'source': {}, 'error': <answer remains>, 'changes':[change_vals, ...]}
+    # where payload['changes'] is a list of dictionaries, each one used as an update dict for a model.
+    parent = _get_report_queue(queue_name, report_settings)
     task = {
             'app_engine_http_request': {  # Specify the type of request.
-                'http_method': http_method,
-                'relative_uri': report_api_path
+                'http_method': 'POST',
+                'relative_uri': report_settings('relative_uri', ''),
+                'body': payload.encode()  # Task API requires type bytes.
             }
     }
-    if payload is not None:
-        task['app_engine_http_request']['body'] = payload.encode()  # Add payload to request as required type bytes.
     if task_name:
         task['name'] = task_name.lower()  # The Task API will generate one if it is not set.
     if in_seconds:
